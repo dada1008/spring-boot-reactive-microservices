39f0b65abab948f9f83294012ebf5c4812694d5f
Dadasaheb Patil <dada1008@gmail.com>
2018-10-13 23:13:34 +0530

    Added eureka service registry module, to register microservice and use based on name instead of ip and port

--- account-service/src/main/java/com/opensource/dada/account/repository/AccountRepository.java
+++ account-service/src/main/java/com/opensource/dada/account/repository/AccountRepository.java
@@ -2,7 +2,6 @@ package com.opensource.dada.account.repository;
 
 import com.opensource.dada.account.model.Account;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
 import reactor.core.publisher.Flux;
 
 public interface AccountRepository extends ReactiveCrudRepository<Account, String> {
---
+++ eureka-service-registry/src/main/java/com/opensource/dada/eureka/service/registry/EurekaServiceRegistryApplication.java
@@ -0,0 +1,15 @@
+package com.opensource.dada.eureka.service.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServiceRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceRegistryApplication.class, args);
+    }
+
+}
